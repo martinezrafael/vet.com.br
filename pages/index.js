897c911f0645a167ea7f60d42d3f85e1aff793c8
Rafael Martinez <rafaelmartinez.contato@gmail.com>
2024-01-04 20:16:23 -0300

    add a `.editorconfig` file

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-function Home(){
+function Home() {
   return (
     <>
       <h1>vet.com.br</h1>
-      <p>A maior plataforma de contratação de veterinários do Brasil.</p>
+      <p>A maior plataforma de contratação de veterinários do Brasil.</p>      
     </>
-  )
+  );
 }
 
 export default Home;
